94443395b0bcc2e9813697277c19a27e6eefb2cf
Shri Harshan <132484829+iamshriharshan@users.noreply.github.com>
2025-07-09 16:03:06 +0530

    Fix nested anchor tags in Homepage

---
+++ src/Homepage.jsx
@@ -0,0 +1,12 @@
+@@ .. @@
+            <p className="hero-subtitle">
+              Fostering Innovation & Excellence in Technical Education
+            </p>
+-            <Link to="/"><a className="btn btn-primary mt-3 px-4">
++            <Link to="/" className="btn btn-primary mt-3 px-4">
+              Login
+-            </a></Link>
++            </Link>
+             
+           </div>
+         </section>
